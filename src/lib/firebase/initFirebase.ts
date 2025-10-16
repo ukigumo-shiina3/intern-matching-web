bd5230f53f4c341089a687514f9a8ef688800a46
ukigumo-shiina3 <daisukeyao@outlook.jp>
2025-10-16 22:58:30 +0900

    feat: 🎸 Firebaseを初期化

---
+++ src/lib/firebase/initFirebase.ts
@@ -0,0 +1,17 @@
+import { getAnalytics } from 'firebase/analytics'
+import { initializeApp } from 'firebase/app'
+
+const firebase = initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+})
+
+if (typeof window !== 'undefined') {
+  getAnalytics(firebase)
+}
+
+export default firebase
